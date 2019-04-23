4e223f3f036337df72c88b4a21facd607c881c64
ellevalentine <elle.valentine@hotmail.com>
2019-04-23 10:44:23 +0100

    done

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)  # this will be run whenever there is a request received
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is"
     resp.finish
   end
 
